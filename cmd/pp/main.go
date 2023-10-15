f1b21f108b7c4749d75651d01d3af8a37efbc910
realwaydao <141410713+realwaydao@users.noreply.github.com>
2023-10-15 12:03:05 +0000

    feat main

--- cmd/pp/main.go
+++ cmd/pp/main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/panjf2000/gnet/v2"
+)
+
+type echoServer struct {
+	gnet.BuiltinEventEngine
+
+	eng       gnet.Engine
+	addr      string
+	multicore bool
+}
+
+func (es *echoServer) OnBoot(eng gnet.Engine) gnet.Action {
+	es.eng = eng
+	log.Printf("echo server with multi-core=%t is listening on %s\n", es.multicore, es.addr)
+	return gnet.None
+}
+
+func (es *echoServer) OnTraffic(c gnet.Conn) gnet.Action {
+	buf, _ := c.Next(-1)
+	c.Write(buf)
+	log.Printf("echo server with multi-core=%t is listening on %s\n", es.multicore, es.addr)
+	return gnet.None
+}
 
 func main() {
-	fmt.Println("hello")
+	var port int
+	var multicore bool
+
+	// Example command: go run echo.go --port 9000 --multicore=true
+	flag.IntVar(&port, "port", 9000, "--port 9000")
+	flag.BoolVar(&multicore, "multicore", false, "--multicore true")
+	flag.Parse()
+	echo := &echoServer{addr: fmt.Sprintf("tcp://0.0.0.0:%d", port), multicore: multicore}
+	log.Fatal(gnet.Run(echo, echo.addr, gnet.WithMulticore(multicore)))
 }
