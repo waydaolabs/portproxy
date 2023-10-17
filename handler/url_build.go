c8f0b9277f0a238a9f6f345d582203c961953ab7
realwaydao <141410713+realwaydao@users.noreply.github.com>
2023-10-17 13:52:43 +0000

    url build

--- handler/url_build.go
+++ handler/url_build.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -11,11 +13,17 @@ func UrlBuild(c *fiber.Ctx) error {
 		return err
 	}
 
+	if len(data.Id) < 1 {
+		return errors.New("id error")
+	}
+	if len(data.Host) < 1 {
+		return errors.New("host error")
+	}
+
 	SetUrls(data)
 	return c.JSON(GetUrls())
 }
 
 func GetUrlBuild(c *fiber.Ctx) error {
-
 	return c.JSON(GetUrls())
 }
